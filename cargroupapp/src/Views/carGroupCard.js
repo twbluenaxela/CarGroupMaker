e0be37666803e16e9177b623ed570ed1fd468951
Nick <rednaxela64@outlook.com>
2022-07-02 23:29:45 +0000

    Change UI of card

--- cargroupapp/src/Views/carGroupCard.js
+++ cargroupapp/src/Views/carGroupCard.js
@@ -45,81 +45,78 @@ export default function CarGroupCard() {
   };
 
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Car Group 车组
-        </Typography>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="outlined-basic"
-            label="Car Group # 车组号码"
-            variant="outlined"
-            size="small"
-          />
-          <br />
-          <TextField
-            id="standard-basic"
-            label="Territory Number 地区号"
-            variant="standard"
-            size="small"
-          />
-          <Typography variant="h7" component="div">
-            人员 People
-            <IconButton
-              aria-label="add people"
-              size="small"
-              onClick={handleAddPeopleInputField}
-            >
-              <AddIcon fontSize="inherit" />
-            </IconButton>
+    <Grid container
+    spacing={0}
+    xs={12}
+    direction="column"
+    alignItems="center"
+    justify="center"
+    style={{ minHeight: '90vh', paddingTop: "20px" }}>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Car Group 车组
           </Typography>
-          {peopleInputFields.map((input, index) => {
-            return (
-              <Box key={index}>
-                <TextField
-                  id="person"
-                  label="Name 姓名"
-                  variant="standard"
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton edge="end" color="primary" onClick={() => handleRemovePeopleInputField(index)}>
-                          <DeleteIcon />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                  value={input.name}
-                  onChange={(event) => handleFormChange(index, event)}
-                />
-              </Box>
-            );
-          })}
-        </Box>
-
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="outlined-basic"
+              label="Car Group # 车组号码"
+              variant="outlined"
+              size="small"
+            />
+            <br />
+            <TextField
+              id="standard-basic"
+              label="Territory Number 地区号"
+              variant="standard"
+              size="small"
+            />
+            <Typography variant="h7" component="div">
+              人员 People
+              <IconButton
+                aria-label="add people"
+                size="small"
+                onClick={handleAddPeopleInputField}
+              >
+                <AddIcon fontSize="inherit" />
+              </IconButton>
+            </Typography>
+            {peopleInputFields.map((input, index) => {
+              return (
+                <Box key={index}>
+                  <TextField
+                    id="person"
+                    label="Name 姓名"
+                    variant="standard"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            edge="end"
+                            color="primary"
+                            onClick={() => handleRemovePeopleInputField(index)}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                    value={input.name}
+                    onChange={(event) => handleFormChange(index, event)}
+                  />
+                </Box>
+              );
+            })}
+          </Box>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }
