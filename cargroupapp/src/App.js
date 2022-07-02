18112bd767e54c4fdc0306c8a5a890e34936eb8f
Nick <rednaxela64@outlook.com>
2022-07-02 21:45:44 +0000

    Add default card

--- cargroupapp/src/App.js
+++ cargroupapp/src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
 import ButtonAppBar from './Views/ButtonAppBar'
 import AddCarGroupButton from './Views/addCarGroupButton'
+import CarGroupCard from './Views/carGroupCard'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div>
       <ButtonAppBar />
       <AddCarGroupButton />
+      <CarGroupCard />
       
     </div>
   );
