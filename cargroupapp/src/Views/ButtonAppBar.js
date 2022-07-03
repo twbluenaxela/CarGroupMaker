e06ad4272e78808ee16dabe6f9e0fb40d45d9ea3
Nick <rednaxela64@outlook.com>
2022-07-03 00:37:17 +0000

    Add drawer

--- cargroupapp/src/Views/ButtonAppBar.js
+++ cargroupapp/src/Views/ButtonAppBar.js
@@ -6,8 +6,71 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 
 export default function ButtonAppBar() {
+
+  const [drawerState, setDrawerState] = React.useState({top: false,
+    left: false,
+    bottom: false,
+    right: false,})
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+    setDrawerState({ ...drawerState, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+    sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer()}
+      onKeyDown={toggleDrawer()}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -17,6 +80,7 @@ export default function ButtonAppBar() {
             edge="start"
             color="inherit"
             aria-label="menu"
+            onClick={toggleDrawer('left', true)}
             sx={{ mr: 2 }}
           >
             <MenuIcon />
@@ -26,6 +90,17 @@ export default function ButtonAppBar() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <React.Fragment key={'left'}>
+    <SwipeableDrawer
+      anchor={'left'}
+      open={drawerState['left']}
+      onClose={toggleDrawer('left', false)}
+      onOpen={toggleDrawer('left', true)}
+    >
+      {list('left')}
+    </SwipeableDrawer>
+  </React.Fragment>
     </Box>
+    
   );
 }
